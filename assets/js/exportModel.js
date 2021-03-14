5b49876d9fefca79930c2a834d03a33f7b31856c
朱志康 <752330461@qq.com>
2021-03-14 23:06:50 +0800

    实现在屏幕上显示视频，后续关注根据后端返回坐标确认显示器与屏幕位置。

--- assets/js/exportModel.js
+++ assets/js/exportModel.js
@@ -1,8 +1,26 @@
 import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
 
-export {setupModel_scene_ani0,setupModel_scene_ani2,setupModel_scene_ani1,
-    
-    setupModel_child_ani0}
+export {
+    setupModel_scene_ani0, setupModel_scene_ani2, setupModel_scene_ani1,
+
+    setupModel_child_ani0,
+    createCube
+}
+
+function createCube(size) {
+    const geometry = new THREE.BoxGeometry(...size);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    const radiansPerSecond = THREE.MathUtils.degToRad(30);
+    console.log('radiansPerSecond', radiansPerSecond);
+    // each frame, rotate the entire group of spheres
+    cube.tick = (delta) => {
+        // console.log('delta',delta);
+        cube.rotation.x += delta * radiansPerSecond;
+        cube.rotation.y += delta * radiansPerSecond;
+    };
+    return cube;
+}
 
 function setupModel_scene_ani0(data) {
     const model = data.scene;
